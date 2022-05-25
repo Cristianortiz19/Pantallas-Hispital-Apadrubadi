815cbe53ff68f28d9a98a1a31ff3cf917f356e02
Cristianortiz19 <cristclar012@gmail.com>
2022-05-24 23:26:39 -0500

    a

--- sketch.js
+++ sketch.js
@@ -18,3 +18,23 @@ function preload() {
   pantallas[2] = loadImage("assets/Pantalla2.png");
   pantallas[3] = loadImage("assets/Pantalla3.png");
 }
+
+function mousePressed() {
+  // Botón iniciar sección
+  if(mouseX > 35 && mouseX < 35 + 360 && mouseY > 540 && mouseY < 540 + 45 &&
+    pantallaActual === 0){
+    pantallaActual = 1;
+  }
+
+  // Botón de solicitar asistencia
+  if(mouseX > 60 && mouseX < 60 + 305 && mouseY > 142 && mouseY < 142 + 55 &&
+    pantallaActual === 1){
+    pantallaActual = 2;
+  }
+
+  // Botón de urgencia BAJA
+  if(mouseX > 55 && mouseX < 55 + 312 && mouseY > 427 && mouseY < 427 + 42 &&
+    pantallaActual === 2){
+    pantallaActual = 3;
+  }
+}
